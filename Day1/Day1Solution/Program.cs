d50969f3afada4a5b278f7c47a34c5c39b84bbed
aleksahadzic22 <aki.hadza@gmail.com>
2025-05-30 17:09:19 +0200

    Day1_Part2

--- Day1/Day1Solution/Program.cs
+++ Day1/Day1Solution/Program.cs
@@ -8,7 +8,15 @@ namespace Day1Solution
         {
             (List<int> col1, List<int> col2) lists = ExtractTextToList();
 
-            int difference = CalculateDifferences(lists.col1, lists.col2);
+            int difference = Part1_CalculateDifferences(lists.col1, lists.col2);
+
+            int difference2 = Part1_CalculateDifferences_MoreElegant(lists.col1, lists.col2);
+
+            int similarityScore = Part2_SimilarityScore(lists.col1, lists.col2);
+
+            int similarityScore2 = Part2_SimilarityScore_MorePerformant_Perhaps(lists.col1, lists.col2);
+
+
         }
 
 
@@ -41,7 +49,7 @@ namespace Day1Solution
         }
         
 
-        private static int CalculateDifferences(List<int> list1, List<int> list2)
+        private static int Part1_CalculateDifferences(List<int> list1, List<int> list2)
         {
             int difference = 0;
 
@@ -51,6 +59,58 @@ namespace Day1Solution
             }
             return difference;
 
+            //I think this is O(n)
+
+            //An Edge Case here is what if they are different lengths, these 2 collections. We might need to append 0s at the end of the shorter one, in that case?
+
+        }
+
+        private static int Part1_CalculateDifferences_MoreElegant(List<int> list1, List<int> list2)
+        {
+            return list1.Zip(list2, (a, b) => Math.Abs(a - b)).Sum();
+
+            //I think this is also O(n) but I'm not sure.
+        }
+
+        private static int Part2_SimilarityScore(List<int> list1, List<int> list2)
+        {
+            int similarityScore = 0;
+
+            for(int i = 0; i < list1.Count; i++)
+            {
+                similarityScore += list1[i] * list2.Where(x => x == list1[i]).Count();
+            }
+
+            return similarityScore;
+
+            //O(n * m)
+
+
+        }
+
+        private static int Part2_SimilarityScore_MorePerformant_Perhaps(List<int> list1, List<int> list2)
+        {
+            var frequency = new Dictionary<int, int>();
+            foreach (var num in list2)
+            {
+                if (frequency.ContainsKey(num))
+                    frequency[num]++;
+                else
+                    frequency[num] = 1;
+            }
+             int similarityScore = 0;
+
+            foreach (var num in list1)
+            {
+                if (frequency.TryGetValue(num, out int count))
+                {
+                    similarityScore += num * count;
+                }
+            }
+
+            //O(n + m)
+
+            return similarityScore;
         }
 
 
