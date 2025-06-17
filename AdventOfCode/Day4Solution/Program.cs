175bf93f5643bd305d3d8d83d704966fc8387058
aleksahadzic22 <aki.hadza@gmail.com>
2025-06-17 13:12:37 +0200

    day5 impl

--- AdventOfCode/Day4Solution/Program.cs
+++ AdventOfCode/Day4Solution/Program.cs
@@ -7,6 +7,9 @@ namespace Day4Solution
         static void Main(string[] args)
         {
             char[][] input = ReceiveInput();
+
+            //int occurences = Traverse(input);
+            int occurences = TraverseMAS(input);
         }
 
         private static char[][] ReceiveInput()
@@ -18,35 +21,116 @@ namespace Day4Solution
             return lines.Select(line => line.ToCharArray()).ToArray();
         }
 
-        private static void Traverse(char[][] input)
+        private static int TraverseMAS(char[][] input)
         {
             (int dx, int dy)[] directions = new[]
             {
-                (1, 0),   // right
-                (0, 1),   // down
-                (1, 1),   // down-right
-                (-1, 1)   // down-left
+                (1, 1),    // ️down-right
+                (-1, -1),  // ️up-left
+                (-1, 1),   // ️down-left
+                (1, -1)    // ️up-right
             };
+            int foundCount = 0;
+
             for(int y = 0; y < input.Length; y++)
             {
                 for(int x = 0; x < input[y].Length; x++)
                 {
-                    foreach(var (dx,dy) in directions)
+                    if (!(input[y][x] == 'A')) continue;
+                    List<bool> matchesDiagonal = new();
+                    
+                    foreach (var (dx, dy) in directions)
                     {
-                        if (MatchesPattern(input, x, y, dx, dy, "XMAS"))
-                        {
+                        int x1 = x + dx, y1 = y + dy;
 
+                        int x2 = x - dx, y2 = y - dy;
+
+                        if(IsIndexInBounds(x1,x2,y1,y2,input))
+                        {
+                            matchesDiagonal.Add(LettersMatch(input[y1][x1], input[y2][x2]));
+                        }
+                    }
+                    if(matchesDiagonal.Count() == 4 && matchesDiagonal.All(x => x == true))
+                    {
+                        foundCount++;
+                    }
+                }
+            }
+
+            return foundCount;
+        }
+
+
+        static bool LettersMatch(char letter1, char letter2)
+        {
+            return (letter1 == 'M' && letter2 == 'S') || (letter1 == 'S' && letter2 == 'M');
+        }
+
+
+        static bool IsIndexInBounds(int x1, int x2, int y1, int y2, char[][] input)
+        {
+            if (y1 < 0 || y1 >= input.Length || y2 < 0 || y2 >= input.Length)
+                return false;
+
+            if (x1 < 0 || x1 >= input[y1].Length || x2 < 0 || x2 >= input[y2].Length)
+                return false;
+
+            return true;
+        }
+
+        private static int Traverse(char[][] input)
+        {
+            (int dx, int dy)[] directions = new[]
+            {
+                (1, 0),    // ️right
+                (-1, 0),   // ️left
+                (0, 1),    // ️down
+                (0, -1),   // ️up
+                (1, 1),    // ️down-right
+                (-1, -1),  // ️up-left
+                (-1, 1),   // ️down-left
+                (1, -1)    // ️up-right
+            };
+            int foundCount = 0;
+            for (int y = 0; y < input.Length; y++)
+            {
+                for (int x = 0; x < input[y].Length; x++)
+                {
+                    foreach (var (dx, dy) in directions)
+                    {
+                        if (MatchesPattern(input, x, y, dx, dy,"XMAS"))
+                        {
+                            foundCount++;
                         }
                     }
                 }
 
             }
+            return foundCount;
 
         }
 
-        private static bool MatchesPattern(char[][] grid, int x, int y, int dx, int dy, string v)
+        private static bool MatchesPattern(char[][] grid, int x, int y, int dx, int dy, string word)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < word.Length; i++)
+            {
+                var nx = x + dx * i;
+
+                var ny = y + dy * i;
+
+
+                if (ny < 0 || ny >= grid.Length || nx < 0 || nx >= grid[0].Length)
+                {
+                    return false;
+                }
+
+                if (grid[ny][nx] != word[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
---
+++ AdventOfCode/Day5Solution/Program.cs
@@ -0,0 +1,177 @@
+﻿using System.Threading.Tasks;
+
+namespace Day5Solution
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string pairsInput = @"
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13";
+            var tupleList = RetrieveInput(pairsInput);
+
+            var strInputList = @"75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47";
+
+            var orderedList = GetOrderedLists(strInputList);
+
+            var midCount = CheckForCount(orderedList, tupleList);
+
+            var count = CountMiddles(midCount);
+        }
+
+        private static int CountMiddles(List<List<int>> validInputs)
+        {
+            int count = 0;
+
+            foreach(var input in validInputs)
+            {
+                count += input[input.Count / 2];
+            }
+
+            return count;
+
+        }
+
+        private static List<List<int>> CheckForCount(List<List<int>> input1, List<Tuple<int,int>> input2)
+        {
+            List<List<int>> validLists = new();
+            foreach(var itemList in input1)
+            {
+                if(isInputListValid(itemList, input2))
+                {
+                    validLists.Add(itemList);
+                }
+            }
+
+            return validLists;
+           
+        }
+
+        private static bool OneOfTheItemsIsInTuple(Tuple<int, int> tuple, int item)
+        {
+            return item == tuple.Item1 || item == tuple.Item2;
+        }
+
+        private static bool OneOfTheItemsIsInList(List<int> items, Tuple<int, int> tuple)
+        {
+            return items.Contains(tuple.Item1) && items.Contains(tuple.Item2);
+        }
+
+
+        private static bool isInputListValid(List<int> itemList, List<Tuple<int, int>> input2)
+        {
+            List<bool> isValid = new();
+            foreach (var item in itemList) {
+                var allRulesThatMentionThisItem = new List<Tuple<int,int>>();
+                foreach (var tupleElement in input2)
+                {
+                    if (OneOfTheItemsIsInTuple(tupleElement, item) && OneOfTheItemsIsInList(itemList,tupleElement))
+                    {
+                        allRulesThatMentionThisItem.Add(tupleElement);
+                    }
+
+                }
+                bool itPassesAllRules = ItPassesAllRules(item,allRulesThatMentionThisItem,itemList);
+
+                isValid.Add(itPassesAllRules);
+
+            }
+
+            return isValid.All(x => x);
+        }
+
+        private static bool ItPassesAllRules(int item,List<Tuple<int,int>> allRulesThatMentionThisItem,List<int> itemList)
+        {
+            List<bool> allRules = new();
+
+            foreach(var rule in allRulesThatMentionThisItem)
+            {
+                bool pass = false;
+                if (rule.Item1 == item)
+                {
+                    pass = itemList.IndexOf(item) < itemList.IndexOf(rule.Item2);
+                }
+                else if (rule.Item2 == item)
+                {
+                    pass = itemList.IndexOf(item) > itemList.IndexOf(rule.Item1);
+                }
+                allRules.Add(pass);
+            }
+
+            return allRules.All(x => x);
+
+        }
+
+        private static List<Tuple<int, int>> RetrieveInput(string pairsInput)
+        {
+            return pairsInput
+                .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(line =>
+                {
+                    var parts = line.Trim().Split('|');
+                    return Tuple.Create(int.Parse(parts[0]), int.Parse(parts[1]));
+                })
+                .ToList();
+        }
+
+        private static List<List<int>> GetOrderedLists(string input)
+        {
+            char[] delimiters = [',', '\r','\n'];
+
+            var strArr = input.Split(delimiters);
+
+            List<List<int>> result = new();
+
+            var current = new List<int>();
+
+            foreach(var item in strArr)
+            {
+                if (item == "")
+                {
+                    if(current.Count > 0)
+                    {
+                        result.Add(new List<int>(current));
+                        current.Clear();
+                    }
+                }
+                else
+                {
+                    current.Add(int.Parse(item));
+                }
+            }
+
+            return result;
+        }
+
+        private static string ParseRawInputFromText()
+        {
+            string fileName = "input1.txt";
+
+
+        }
+    }
+}
